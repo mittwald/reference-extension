620c0d589a070149f0b9e74e8037796fed0b47f6
Frederic Reisenhauer <f.reisenhauer@mittwald.de>
2025-11-10 12:29:13 +0100

    setup docker, docker compose and docker based development environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { nitroV2Plugin } from "@tanstack/nitro-v2-vite-plugin";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -5,6 +6,7 @@ import viteTsConfigPaths from "vite-tsconfig-paths";
 
 const config = defineConfig({
     plugins: [
+        nitroV2Plugin(),
         // this is the plugin that enables path aliases
         viteTsConfigPaths({
             projects: ["./tsconfig.json"],
