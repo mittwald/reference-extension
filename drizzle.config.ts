de6005aac50156d5804f2f0993ccdb2b837dd293
Frederic Reisenhauer <f.reisenhauer@mittwald.de>
2025-11-11 11:38:54 +0100

    set up drizzle and add example schema for extension instancess

---
+++ drizzle.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "drizzle-kit";
+import { getEnvironmentVariables } from "@/env.ts";
+
+const env = getEnvironmentVariables();
+
+export default defineConfig({
+    out: "./drizzle",
+    schema: "./src/db/schema.ts",
+    dialect: "postgresql",
+    dbCredentials: {
+        host: env.POSTGRES_HOST,
+        port: env.POSTGRES_PORT,
+        user: env.POSTGRES_USER,
+        password: env.POSTGRES_PASSWORD,
+        database: env.POSTGRES_DB,
+        ssl: env.POSTGRES_USE_SSL,
+    },
+});
