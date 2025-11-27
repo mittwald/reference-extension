9e5a1a8936c5f142355556a387bcc0bf2bcd3fa8
Tim Dellmann <t.dellmann@mittwald.de>
2025-11-27 09:40:01 +0100

    move frontend fragment part into seperate file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,5 @@
-import { ColumnLayout, Section } from "@mittwald/flow-remote-react-components";
-import { Title } from "@mittwald/mstudio-ext-react-components";
 import { createFileRoute } from "@tanstack/react-router";
-import { APIReferenceCard } from "@/components/APIReferenceCard.tsx";
-import { CommentsCard } from "@/components/comments/CommentsCard";
-import { DeveloperPortalCard } from "@/components/DeveloperPortalCard.tsx";
-import { FlowDocumentationCard } from "@/components/FlowDocumentationCard.tsx";
-import { GreetingCard } from "@/components/GreetingCard.tsx";
-import { ProjectCard } from "@/components/project/ProjectCard.tsx";
-import { ReadmeCard } from "@/components/ReadmeCard.tsx";
+import { MStudioFrontendFragment } from "@/components/MStudioFrontendFragment.tsx";
 
 export const Route = createFileRoute("/")({
     component: App,
@@ -16,23 +8,6 @@ export const Route = createFileRoute("/")({
 
 function App() {
     return (
-        <>
-            <Title>Hallo Contributor!</Title>
-            <Section>
-                <ColumnLayout m={[1]} l={[3, 2]}>
-                    <ColumnLayout m={[1]}>
-                        <GreetingCard />
-                        <ProjectCard />
-                        <CommentsCard />
-                    </ColumnLayout>
-                    <ColumnLayout m={[1]}>
-                        <DeveloperPortalCard />
-                        <APIReferenceCard />
-                        <FlowDocumentationCard />
-                    </ColumnLayout>
-                </ColumnLayout>
-                <ReadmeCard />
-            </Section>
-        </>
+        <MStudioFrontendFragment />
     );
 }
